70a925248c3beb4166a070528c229cf76c4a6c5b
infernusmode <ezequielpratisoli@gmail.com>
2022-12-02 11:21:31 -0300

    solved

---
+++ problems/04-remove-e-words-filter.js
@@ -0,0 +1,28 @@
+/*
+Write a function `removeEWords(sentence)` that accepts a sentence string as an
+arg. The function should return a new string, containing only the words that
+don't have the letter "e" in them.
+
+Solve this using Array's `filter()` method.
+
+Examples:
+
+console.log(removeEWords('What time is it everyone?')); // 'What is it'
+console.log(removeEWords('Enter the building')); // 'building'
+
+*/
+
+let removeEWords = function(sentence) {
+    // Your code here
+    let char = sentence.split(" ");
+    let remove = char.filter(i => !i.includes("e"));
+    return remove.join(" ");
+};
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+  module.exports = removeEWords;
+} catch (e) {
+  module.exports = null;
+}
---
+++ problems/11-has-three-vowels.js
@@ -0,0 +1,38 @@
+/*
+Write a function `hasThreeVowels` that accepts a string as an argument.
+The function should return a boolean indicating whether or not the string
+contains at least three different vowels.
+
+Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
+methods.
+
+Examples:
+
+console.log(hasThreeVowels('delicious'));       //  true
+console.log(hasThreeVowels('bootcamp prep'));   //  true
+console.log(hasThreeVowels('bootcamp'));        //  false
+console.log(hasThreeVowels('dog'));             //  false
+console.log(hasThreeVowels('go home'));         //  false
+
+*/
+
+let hasThreeVowels = function(string) {
+    // Your code here
+    let count = 0;
+    string.toLowerCase().split("").forEach(elem => {
+        if(elem == "a" || elem == "e" || elem == "i" || elem == "o" || elem == "u") {
+            count++;
+        }
+    });
+    return count > 3;
+};
+
+// Your code here
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+    module.exports = hasThreeVowels;
+} catch (e) {
+    module.exports = null;
+}
